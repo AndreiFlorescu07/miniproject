20a1f92eb68d67546aba97c6ab1ae8b051ee7991
AndreiFlorescu07 <78689311+AndreiFlorescu07@users.noreply.github.com>
2021-02-09 21:22:21 +0200

    Create java.js

---
+++ java.js
@@ -0,0 +1,4 @@
+//un prompt unde se cer date si se salveaza intr o variabila dialogBox;
+var dialogBox = prompt('Daca doriti sa mi trimiteti un mesaj, puteti sa o faceti utilizand casuta de mai jos.');
+
+alert('Multumesc!' );
